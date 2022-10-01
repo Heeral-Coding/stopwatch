d80cc5e6fcb98c47e766f0d3bf6da6f4b2a474a8
Heeral-Coding <91391992+Heeral-Coding@users.noreply.github.com>
2022-10-01 14:13:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(600, 600);
+    angleMode(DEGREES);
+}
+
+function draw() {
+    background(0);
+    translate(300, 300);
+    rotate(-90);
+
+    // set up
+
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+    let ms = millis();
+
+    
+}
